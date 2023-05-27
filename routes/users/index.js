1edf679d91df3cfbe85df832b7e47b20b5205f33
Thabiso <therealhackingknight@gmail.com>
2023-05-27 09:44:26 +0200

    return track url hashed

--- routes/users/index.js
+++ routes/users/index.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const { ArtistModel } = require("../../models/artist_model");
 const { TrackModel } = require("../../models/track_model");
 const { requestErr} = require("../../utils/functions")
+const jwt = require("jsonwebtoken");
+
 router.get("/", async (req, res) => {
   const { username } = req.query;
 
@@ -11,13 +13,14 @@ router.get("/", async (req, res) => {
     if (!data) return res.status(404).json(requestErr("User with specified username not found"))
     let tracks = [];
     let plays = 0, likes = 0, shares = 0;
-    for (let trackId of data.tracks) {
+    for (let trackId of data.tracks) { // Clean up each track
         
       let track = await TrackModel.findById(trackId).exec();
       plays += track.plays.length;
       likes += track.likes.length;
       shares += track.shares.length;
-      track = { ...track.toObject(), id: track.id, artist: data.toObject() };
+      let hashedURL = jwt.sign(track.url, process.env.SECRET_KEY);
+      track = { ...track.toObject(), id: track.id, artist: data.toObject(), url: hashedURL };
       tracks.push(track)
     }
     data = { ...data.toObject(), id: data.id, tracks, plays, likes, shares };
