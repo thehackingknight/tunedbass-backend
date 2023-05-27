de1092984543c8ece66447fcd5489c8662d6d814
Thabiso <therealhackingknight@gmail.com>
2023-05-27 02:34:57 +0200

    send track

--- models/artist_model.js
+++ models/artist_model.js
@@ -6,6 +6,9 @@ const ArtistSchema = new Schema({
         required: true,
         unique: true
     },
+    address: {
+        type: String,
+    },
     email: {
         type: String,
         required: true,
--- routes/users/index.js
+++ routes/users/index.js
@@ -1,28 +1,49 @@
-const router = require('express').Router()
-const { ArtistModel } = require('../../models/artist_model')
-const { TrackModel } = require('../../models/track_model')
+const router = require("express").Router();
+const { ArtistModel } = require("../../models/artist_model");
+const { TrackModel } = require("../../models/track_model");
 
-router.get("/", (req, res) =>{
-    ArtistModel.find().then(_res=>{
-        res.json({"users": _res})
-    })
-})
+router.get("/", async (req, res) => {
+  const { username } = req.query;
 
-router.post('/delete', async (req, res)=>{
-    const {username} = req.body
-    ArtistModel.findOneAndRemove({ username }).then(user =>{
-        if (user){
-            // Delete tracks uploaded by this user
-            user.tracks.forEach(it=>{
-                TrackModel.findByIdAndRemove(it).then(r=>{
-                    if (r) console.log("Removed Track: " + r.title);
-                })
-            })
-      
-            res.status(200).send(user.username + " deleted!")}
-        else res.status(400).send("User not found")
-    })
-})
+  let data;
+  if (username) {
+    data = await ArtistModel.findOne({ username }).exec();
+    let tracks = [];
+    let plays = 0, likes = 0, shares = 0;
+    for (let trackId of data.tracks) {
+        
+      let track = await TrackModel.findById(trackId).exec();
+      plays += track.plays.length;
+      likes += track.likes.length;
+      shares += track.shares.length;
+      track = { ...track.toObject(), id: track.id, artist: data.toObject() };
+      tracks.push(track)
+    }
+    data = { ...data.toObject(), id: data.id, tracks, plays, likes, shares };
+  } else {
+    data = await ArtistModel.find().exec();
+    data = data.map((it) => {
+      return { ...it.toObject(), id: it.id };
+    });
+  }
 
+  res.status(200).json({ data: data });
+});
 
-module.exports = router
+router.post("/delete", async (req, res) => {
+  const { username } = req.body;
+  ArtistModel.findOneAndRemove({ username }).then((user) => {
+    if (user) {
+      // Delete tracks uploaded by this user
+      user.tracks.forEach((it) => {
+        TrackModel.findByIdAndRemove(it).then((r) => {
+          if (r) console.log("Removed Track: " + r.title);
+        });
+      });
+
+      res.status(200).send(user.username + " deleted!");
+    } else res.status(400).send("User not found");
+  });
+});
+
+module.exports = router;
